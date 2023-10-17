8f522f8d99f5f8b5ab573a7d65227a1889391214
Shashant <shashantchaudhary999@gmail.com>
2023-10-17 19:04:17 +0530

    PhaseSection4.9

---
+++ Phase1Section4.9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        public static void runApp()
+        {
+            Stack stack = new Stack();
+            Console.WriteLine($"Length of the Stack {stack.Length}");
+            for (char  i = 'A'; i < 'J'; i++)
+            {
+                Node node = new Node();
+                node.Data=i.ToString();
+                stack.push(node);
+                Console.WriteLine($"Length of the Stack {stack.Length}");
+            }
+            stack.pop();
+            Node node1 = new Node();
+            node1.Data = "J";
+            stack.push(node1);
+            Console.WriteLine(stack.listContents());
+            Console.WriteLine($"Stack Peek element is");
+            stack.peek();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Phase1Section4.9/Stack.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._9
+{
+    internal class Stack
+    {
+        private Node root = null;
+        private Node current = null;
+        private int length;
+
+        public Node Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+        public Node Current
+        {
+            get { return current; }
+            set { current = value; }
+        }
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public Stack() { }
+        public void  push(Node node) {
+        
+        if(root == null)
+            {
+            root = node;
+            current = node;
+            length=1;
+            root.Prev = null;
+            }
+            else
+            {
+            current.Next = node;
+            node.Prev = current;
+            current = node;
+                length++;
+            }
+        }
+        public void pop() { 
+        
+        if(root == null)
+            {
+                Console.WriteLine("Stack is empty!!!!");
+                return;
+            }
+            else if (current == root)
+            {
+                current=null;
+                root = null;
+                length = 0;
+            }
+            else
+            {
+                Node temp= current.Prev;
+                current.Prev.Next = null;
+                Console.WriteLine($"The last entered data is deleted {current.Data}");
+                current = temp;
+                length--;
+            }
+        }
+        public Node getRoot()
+        {
+            return this.root;
+        }
+        public int getLengthOfStack()
+        {
+            return this.length;
+        }
+        public string listContents()
+        {
+            string retVal = "";
+            Node temp= root;
+            
+            while (temp != null)
+            {
+                retVal += temp.Data + " ";
+                temp = temp.Next;
+            }
+            temp = root;
+            return retVal;
+        }
+
+        public void peek()
+        {
+            if((current==null))
+            {
+                Console.WriteLine("Stack is empty!!!!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(current.Data);
+            }
+        }
+
+    }
+}
