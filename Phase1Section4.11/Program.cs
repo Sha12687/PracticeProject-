a0dc0d75b8005fb9121ac6886ec38f87728c968c
Shashant <shashantchaudhary999@gmail.com>
2023-10-17 21:07:53 +0530

    Phase1Section4.11

---
+++ Phase1Section4.11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        public static void runApp()
+        {
+            Queue Q = new Queue(10);
+
+            Q.enQueue(10);
+            Q.enQueue(20);
+            Q.enQueue(30);
+            Q.enQueue(40);
+            Q.enQueue(50);
+            Q.enQueue(60);
+            Q.enQueue(70);
+            Q.enQueue(80);
+            Q.enQueue(90);
+            Q.enQueue(100);
+            Console.WriteLine(Q.printQueue());
+            Q.delete();
+            Q.delete();
+            Console.WriteLine(Q.printQueue());
+        }
+
+    }
+}
---
+++ Phase1Section4.11/Queue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._11
+{
+    internal class Queue
+    {
+        private int[] ele;
+        private int front;
+        private int rear;
+        private int max;
+
+public Queue(int size) {
+            rear = -1;
+            max=size;
+            front = 0;
+            ele = new int[size];    
+        }
+        public void enQueue(int data)
+        {
+            if (rear < max - 1)
+            {
+                ele[++rear] = data;
+            }
+            else
+                return;
+        }
+        public int  delete() { 
+        if(rear+1 ==front) {
+                Console.WriteLine("Queue is empty!!!");
+                return -1;
+            }
+            else
+            {
+                return ele[front++];
+            }
+        
+        }
+        public string printQueue()
+        {
+            string retVal = "";
+            if (front == rear + 1)
+            {
+                return retVal;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    retVal += " " + ele[i];
+                }
+            }
+            return retVal;
+        }
+    }
+}
